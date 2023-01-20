8f844d87e765c76be93f2bfe424ee4f850fb4f74
Justin Lee <lee.justin001126@gmail.com>
2023-01-19 16:45:54 -0800

    add js logic to seperate the special rules for pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,34 @@
 // Business Logic
-function wordPuzzle(text) {
-  console.log(text)
-  let textSplit = Array.from(text);
+function pigLatin(text) {
+  const testVowels = ["a", "e", "i", "o", "u"]
+  const testQu = ["q", "u"]
+  const letters =["b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "r", "s", "t","v", "w", "x", "y", "z"]
+  const textSplit = text.split(' ')
   console.log(textSplit)
   for (i = 0; i <= textSplit.length - 1; i++) {
-    // Checks if the current index is equal to a vowl
-    if (textSplit[i].toLowerCase() === "a" || textSplit[i].toLowerCase() === "e" || textSplit[i].toLowerCase() === "i" || textSplit[i].toLowerCase() === "o" || textSplit[i].toLowerCase() === "u") {
-      textSplit[i] = "-";
-    };
-  };
-  let completeSentence = textSplit.join('');
-  return completeSentence
+    console.log(textSplit[i].charAt(0))
+    if (testQu.includes(textSplit[i].charAt(0)) && testQu.includes(textSplit[i].charAt(1))) {
+      console.log("The QU checks properly")
+    }
+    else if (testVowels.includes(textSplit[i].charAt(0))) {
+      console.log("Hey the vowels check properly")
+    }
+    else if (letters.includes(textSplit[i].charAt(0))) {
+      console.log("Hey every other letter works")
+    }
+  }
 };
 
 // UI Logic
 function handleFormSubmission() {
   event.preventDefault();
   const passage = document.getElementById("text-passage").value.replace(/\r\n/g,'\n');
-  // wordPuzzle(passage);
-  const finishedPuzzle = wordPuzzle(passage);
-  document.getElementById("puzzle").innertext = passage;
-  document.getElementById("final-puzzle").innerText = finishedPuzzle;
-};
+  translation = pigLatin(passage)
+
+
+}
 
 window.addEventListener("load", function(){
-  document.querySelector("form#word-scrambler").addEventListener("submit", handleFormSubmission);
+  document.querySelector("form#pig-latin-translator").addEventListener("submit", handleFormSubmission);
 });
 
